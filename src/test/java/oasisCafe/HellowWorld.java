d804b96d510cdae1ea1366bc8ab171c579c92e1c
thaider1 <thaider1@gmail.com>
2021-10-07 12:03:23 -0400

    first commit

---
+++ src/test/java/oasisCafe/HellowWorld.java
@@ -0,0 +1,15 @@
+package oasisCafe;
+
+public class HellowWorld {
+	
+	String name = "John";
+	int age = 16;
+	
+	  public static void main(String args[]){  
+		  String name = "John";
+			int age = 16;
+		  
+		System.out.println(name + age);
+	}
+
+}
