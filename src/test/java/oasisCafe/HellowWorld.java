16549ad6d82448864f2c6d1369121e9a254fcea4
thaider1 <thaider1@gmail.com>
2021-10-07 13:10:51 -0400

    abc

---
+++ src/test/java/oasisCafe/HellowWorld.java
@@ -0,0 +1,17 @@
+package oasisCafe;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class HellowWorld {
+	
+	
+	
+	  public static void main(String args[]){  
+		  String name = "John";
+			int age = 16;
+		  
+		System.out.println(name + age);
+		System.out.println("This is a try out");
+	}
+
+}
